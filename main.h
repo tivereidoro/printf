bd929f45fd03d0c9c6ae4ac74cd81f29e24323a7
KHMalek <khemirimelek@gmail.com>
2022-10-18 11:24:48 -0700

    main.h change

--- main.h
+++ main.h
@@ -41,9 +41,6 @@ int _printf(const char *format, ...);
 /* get_print */
 int (*get_print(char s))(va_list, flags_t *);
 
-/* get_flag */
-int get_flag(char s, flags_t *f);
-
 /* write_funcs */
 int _putchar(char c);
 int _puts(char *str);
