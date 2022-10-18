c6d0bce0e9c8eaad3eb96e5842dbc2568840e42c
KHMalek <khemirimelek@gmail.com>
2022-10-18 11:04:03 -0700

    made some chages to _printf.c

--- _printf.c
+++ _printf.c
@@ -8,42 +8,39 @@
  */
 int _printf(const char *format, ...)
 {
-	int sum = 0;
-	va_list ap;
-	char *p, *start;
-	params_t params = PARAMS_INIT;
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
 
-	va_start(ap, format);
+	flags_t flags = {0, 0, 0};
 
+	register int count = 0;
+
+
+
+	va_start(arguments, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	for (p = (char *)format; *p; p++)
+	for (p = format; *p; p++)
 	{
-		init_params(&params, ap);
-		if (*p != '%')
+		if (*p == '%')
 		{
-			sum += _putchar(*p);
-			continue;
-		}
-		start = p;
-		p++;
-		while (get_flag(p, &params)) /* while char at p is flag char */
-		{
-			p++; /* next char */
-		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
 			p++;
-		if (!get_specifier(p))
-			sum += print_from_to(start, p,
-				params.l_modifier || params.h_modifier ? p - 1 : 0);
-		else
-			sum += get_print_func(p, ap, &params);
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-	_putchar(BUF_FLUSH);
-	va_end(ap);
-	return (sum);
+	_putchar(-1);
+	va_end(arguments);
+	return (count);
 }
