fa16ce54a438f30d8c1262f181a0c2b7192d2dae
KHMalek <khemirimelek@gmail.com>
2022-10-18 12:41:48 -0700

    added print_nubmers and made some other changes

--- print_numbers.c
+++ print_numbers.c
@@ -22,21 +22,6 @@ int print_int(va_list l, flags_t *f)
 	return (res);
 }
 
-/**
- * print_unsigned - prints an unsigned integer
- * @l: va_list of arguments from _printf
- * @f: pointer to the struct flags determining
- * if a flag is passed to _printf
- * Return: number of char printed
- */
-int print_unsigned(va_list l, flags_t *f)
-{
-	unsigned int u = va_arg(l, unsigned int);
-	char *str = convert(u, 10, 0);
-
-	(void)f;
-	return (_puts(str));
-}
 
 /**
  * print_number - helper function that loops through
@@ -82,4 +67,4 @@ int count_digit(int i)
 		d++;
 	}
 	return (d);
-}
+i}
