10f1f2e24d1c9356c60d84d6fb6b29f1077e0d6e
tivereidoro <tivereidoro@yahoo.com>
2022-10-18 19:28:59 -0700

    update project c_files

--- _printf.c
+++ _printf.c
@@ -1,46 +1,66 @@
-#include "holberton.h"
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - prints a formatted string from the main string and other variable
- * arguments passed to it
- *
- * @format: main string
- * Return: returns the total number of characters printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
 {
-	int chars;
-	spec_t fun_list[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"%", print_percent},
-		{"d", print_integer},
-		{"i", print_integer},
-		{"u", unsigned_integer},
-		{"b", print_binary},
-		{"r", print_reversed},
-		{"R", rot13},
-		{"o", print_octal},
-		{"x", print_hex},
-		{"X", print_heX},
-		{"S", print_custom},
-		{"p", print_pointer},
-		{NULL, NULL}
-	};
-	va_list arg_list;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-		if (format == NULL)
-			return (-1);
+	if (format == NULL)
+		return (-1);
 
-		va_start(arg_list, format);
+	va_start(list, format);
 
-		/**
-		 * parse_args - parses the main string and all other paremetes
-		 * it receives
-		 */
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
 
-		chars = parse_args(format, fun_list, arg_list);
-		va_end(arg_list);
-		return (chars);
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
