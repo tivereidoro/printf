7f64d9a092de5cab986b51690746b95e0894e88c
tivereidoro <tivereidoro@yahoo.com>
2022-10-18 18:57:23 -0700

    add task update files

--- _printf.c
+++ _printf.c
@@ -1,48 +1,46 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _printf - prints anything according to a format
- * @format: the format string
+ * _printf - prints a formatted string from the main string and other variable
+ * arguments passed to it
  *
- * Return: number of bytes printed
+ * @format: main string
+ * Return: returns the total number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
+	int chars;
+	spec_t fun_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"u", unsigned_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
+		{"S", print_custom},
+		{"p", print_pointer},
+		{NULL, NULL}
+	};
+	va_list arg_list;
 
-	flags_t flags = {0, 0, 0};
+		if (format == NULL)
+			return (-1);
 
-	register int count = 0;
+		va_start(arg_list, format);
 
+		/**
+		 * parse_args - parses the main string and all other paremetes
+		 * it receives
+		 */
 
-
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+		chars = parse_args(format, fun_list, arg_list);
+		va_end(arg_list);
+		return (chars);
 }
